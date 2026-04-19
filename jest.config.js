f5d8bd950c54f39fbd0856ef4a3e67379bda6736
Sanatate-Ai-buget/upload/main <adryanmoldokkr32@gmail.com>
2026-04-19 20:25:53 +0200

    Initial Pro+ deployment with Remotion video engine

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+  },
+};
