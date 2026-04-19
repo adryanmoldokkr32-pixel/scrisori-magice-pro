f2f020e6638cd4e334edf84d31af20e90c140c2a
Sanatate-Ai-buget/upload/main <adryanmoldokkr32@gmail.com>
2026-04-19 21:06:39 +0200

    Fresh Start - Commit 3: Remotion Engine

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { LoveLetterVideo, LoveLetterProps } from './LoveLetterVideo';
+import { LoveLetterVideo } from './LoveLetterVideo';
 import { VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_FPS, DURATION_IN_FRAMES } from './constants';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <>
-      <Composition
-        id="LoveLetter"
-        component={LoveLetterVideo}
-        durationInFrames={DURATION_IN_FRAMES}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-        defaultProps={{
-          recipientName: 'Dragostea Mea',
-        }}
-      />
-    </>
+    <Composition
+      id="LoveLetter"
+      component={LoveLetterVideo}
+      durationInFrames={DURATION_IN_FRAMES}
+      fps={VIDEO_FPS}
+      width={VIDEO_WIDTH}
+      height={VIDEO_HEIGHT}
+      defaultProps={{ recipientName: 'Dragostea Mea' }}
+    />
   );
 };
