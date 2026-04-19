58066626a5e95cab26f6e87c4eef25f358dcb371
Sanatate-Ai-buget/upload/main <adryanmoldokkr32@gmail.com>
2026-04-19 20:25:56 +0200

    Initial Pro+ deployment with Remotion video engine

---
+++ src/remotion/Root.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Composition } from 'remotion';
+import { LoveLetterVideo, LoveLetterProps } from './LoveLetterVideo';
+import { VIDEO_WIDTH, VIDEO_HEIGHT, VIDEO_FPS, DURATION_IN_FRAMES } from './constants';
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="LoveLetter"
+        component={LoveLetterVideo}
+        durationInFrames={DURATION_IN_FRAMES}
+        fps={VIDEO_FPS}
+        width={VIDEO_WIDTH}
+        height={VIDEO_HEIGHT}
+        defaultProps={{
+          recipientName: 'Dragostea Mea',
+        }}
+      />
+    </>
+  );
+};
